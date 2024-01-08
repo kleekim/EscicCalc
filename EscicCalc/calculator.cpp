488464e81c5eea79713a4576754ef7c3d5744b92
kleekim <149334573+kleekim@users.noreply.github.com>
2024-01-08 11:11:07 +0100

    Ajoutez des fichiers projet.

---
+++ EscicCalc/calculator.cpp
@@ -0,0 +1,17 @@
+#include "calculator.h"
+
+double calculator::Calculate(double x, char oper, double y)
+
+    switch(oper)
+    {
+    case'+':
+        return x + y;
+    case'-':
+        return x - y;
+    case'*':
+        return x * y;
+    case'/': 
+        return x / y;
+    default:
+         return 0.0;
+    }
